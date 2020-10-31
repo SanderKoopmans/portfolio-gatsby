6adae5eb70f6a4ffa8a27013dfcf3eb5a065884c
Sander Koopmans <sandkoop@gmail.com>
2020-10-31 01:21:06 +0100

    testing cred.

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -14,6 +14,7 @@ const Features = () => {
         <li><SpeedIcon /> <H4>Bliksem snel</H4><P>Tegenwoordig heeft iedereen minder tijd of geduld. Jouw website moet hiervoor supersnel laden en reageren. Een snellere website wordt daarbij ook beter beloond door bijvoorbeeld Google met een hogere ranking in de zoekresultaten.</P></li>
         <li><AccessibilityIcon /> <H4>Toegankelijk</H4><P>Een groot gedeelte van de bevolking heeft een beperking in bijvoorbeeld het zien van kleuren. Of een andere vorm waardoor het moeilijker is om met een website te interacteren. Hier hou ik rekening mee in al het werk, zodat iedereen online zichzelf ook welkom voelt!</P></li>
       </StyledList>
+      [test]
     </>
   );
 };
