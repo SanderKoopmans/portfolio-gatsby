76365718dab17898b9c83bddfe6c7dac377affd6
Sander Koopmans <sandkoop@gmail.com>
2020-11-03 22:18:28 +0100

    working on main text

--- src/components/Main/MainSection.jsx
+++ src/components/Main/MainSection.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { H4, P } from '../Font';
 import { StyledList } from '../Features/Features.styled';
+import NotInterestedIcon from '@material-ui/icons/NotInterested';
+import FastForwardIcon from '@material-ui/icons/FastForward';
+import BuildIcon from '@material-ui/icons/Build';
+import PeopleIcon from '@material-ui/icons/People';
 
 const MainSection = () => {
   return (
@@ -8,10 +12,26 @@ const MainSection = () => {
       <H4>Why me?</H4>
       <P>Waarom werken met mij en niemand anders?</P>
       <StyledList>
-        <li>Persoonlijk</li>
-        <li>Maatwerk</li>
-        <li>Snelle service</li>
-        <li>Geen uitbesteding</li>
+        <li>
+          <PeopleIcon />
+          <H4>Persoonlijk</H4>
+          <P></P>
+        </li>
+        <li>
+          <BuildIcon />
+          <H4>Maatwerk</H4>
+          <P></P>
+        </li>
+        <li>
+          <FastForwardIcon />
+          <H4>Snelle service</H4>
+          <P></P>
+        </li>
+        <li>
+          <NotInterestedIcon />
+          <H4>Geen uitbesteding</H4>
+          <P></P>
+        </li>
       </StyledList>
     </>
   );
